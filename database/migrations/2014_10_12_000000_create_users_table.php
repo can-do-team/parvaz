ceaffb78dd00b6e8009dd5321c4288e9420eee95
Nima Heidari <nima.heidari.n@gmail.com>
2024-01-10 18:53:51 +0330

    add column group_id in users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('number')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('area');
+            $table->text('address');
             $table->rememberToken();
             $table->timestamps();
         });
