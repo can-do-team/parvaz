95e690a68efa1a47badca0654072dba33048ee29
Nima Heidari <nima.heidari.n@gmail.com>
2024-01-10 19:02:17 +0330

    add Broadcast model

---
+++ database/migrations/2024_01_10_152940_create_broadcasts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('broadcasts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('address');
+            $table->string('area');
+            $table->string('volume');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('broadcasts');
+    }
+};
