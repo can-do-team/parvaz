3d7da57ab34562c8555995a8601eb5d6c79b9d73
Nima Heidari <nima.heidari.n@gmail.com>
2024-01-10 22:15:57 +0330

    test tailwind

--- resources/js/Pages/Test.jsx
+++ resources/js/Pages/Test.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 
 const Test = () => {
     return (
-        <h1>Test Component</h1>
+        <h1 className="text-3xl font-bold underline bg-blue-700">
+            Hello world!sad
+        </h1>
     )
 }
 
