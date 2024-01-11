f5e865cce1bb9cd11be179336e184b70e33b3c3d
Nima Heidari <nima.heidari.n@gmail.com>
2024-01-11 17:13:32 +0330

    show group in app completed

---
+++ resources/views/group_user.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    <style>
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+    </style>
+</head>
+<body class="bg-black flex justify-center items-center h-[100svh]">
+<div class=" w-[420px] h-[85svh] bg-[#09969A] mx-auto rounded-2xl overflow-y-scroll relative ">
+    <header class="w-full min-h-[10svh] flex justify-center items-center">
+        <h1 class='text-center text-2xl mb-8 text-white  mt-8'>مشاهده برنامه گروه ها</h1>
+        <form action="/logout" method="post">
+            @csrf
+            <button type="submit" href="" class="px-3 text-xs py-1 bg-[#00000030] rounded-2xl absolute left-4 top-4 text-white">خروج</button>
+        </form>
+    </header>
+
+    <div class='h-[75svh] flex flex-col items-start justify-center  bg-[#F3F7FB] w-full mx-auto p-5 pb-[80px] rounded-t-[80px]  overflow-y-scroll'>
+
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4">... </h2>
+            ..
+        </div>
+        @foreach($groups as $group)
+        <div class="w-11/12 mx-auto p-6 bg-white shadow rounded-2xl mb-5">
+            <h2 class="text-[#09969A] text-center mb-4 text-xl">{{$group->title}} </h2>
+            <p class="mb-3 text-center text-[#535353] ">{{$group->details}}</p>
+            <p class="text-xs mt-5 text-[#09969A]">{{$group->time}}</p>
+        </div>
+        @endforeach
+    </div>
+    <footer class="bg-[#09969A] h-[80px] w-full absolute bottom-0 flex justify-around items-center rounded-t-[55px] text-center">
+        <a href="" class="flex flex-col justify-between items-center text-white text-sm p-2 hover:bg-[#00000020] w-3/12 rounded-full">
+            <img src="{{URL::asset('images/user.svg')}}" class="mb-1" alt="">
+            حساب کاربری
+        </a>
+        <a href="/dashboard" class="flex flex-col justify-between items-center text-white text-sm p-2 hover:bg-[#00000020] w-3/12 rounded-full">
+            <img src="{{URL::asset('images/home.svg')}}" class="mb-1" alt="">
+            خانه
+        </a>
+        <a href="" class="flex flex-col justify-between items-center text-white text-sm p-2 hover:bg-[#00000020] w-3/12 rounded-full">
+            <img src="{{URL::asset('images/chat.svg')}}" class="mb-1" alt="">
+            پشتیبانی
+        </a>
+    </footer>
+</div>
+</body>
+</html>
